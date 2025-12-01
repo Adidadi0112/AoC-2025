88a5ec37a493b8dcac8f56675f6523e4c4e28593
Adam <adamgruda1312@gmail.com>
2025-12-01 17:28:55 +0100

    Second task from day one completed, but entirely changed solution logic, becasue previous one was too complicated

--- day_1/safe_dial.py
+++ day_1/safe_dial.py
@@ -1,39 +1,38 @@
-from typing import Any, List
+from typing import List
 
 class SafeDial:
 
     def __init__(self):
-        self.dial = list(range(100)) # a list from 0 to 99, which represents safe dial from task description
+        self.dial_size: int = 100 # size of dial [0-99]
         self.current_number = 50 # actual selected number on dial, initial is 50
         self.zeros_sum = 0
 
-    # a parser method for input data - it just create a list from text file and replace L with - and delete R
-    def parse(self, input_data: List[str]):
-        rotation_sequence = [number.replace("R", "") for number in input_data]
-        rotation_sequence = [number.replace("L", "-") for number in rotation_sequence]
-        rotation_sequence = [int(number) for number in rotation_sequence]
-        return rotation_sequence
+    def parse(self, input_data: List[str]) -> List[int]:
+        parsed_input = []
+        for line in input_data:
+            val_str = line.strip().replace("R", "").replace("L", "-")
+            parsed_input.append(int(val_str))
+        return parsed_input
 
-    # setter for current number
-    def set_current_number(self, new_number):
-        self.current_number = self.dial[new_number]
+    def rotate(self, rotation: int) -> None:
+        '''
+        Optimized rotate function, which compares start position, our current_number, with end position, which is 
+        sum of current_number and rotation value. Using '//' operator, function checks how many times dial fully rotates.
+        If 0 was passed, left side of substraction should be higher than the right side by the value of fully crossed dial. eg. current_number = 289, dial_size = 100 and rotation = 540. end_pos // dial_size is 8 and start_pos //
+        dial_size is 2 -> so zero was passed 6 times. If the result of equation is 0 - then 0 was not passed on dial. 
+        '''
+        start_pos = self.current_number
+        end_pos = start_pos + rotation
 
-    '''
-    rotate method
+        passed_zeros = 0
 
-    accepts rotation: int
-
-    if rotation and current number on dial is higher than entire dial (more than 100), then 
-    it's calculated module from dividing rotation and current number by dial length. Thanks to that
-    We can avoid out of bounds error
-    '''
-    def rotate(self, rotation):
-        if rotation + self.current_number > len(self.dial) - 1 or rotation + self.current_number < -len(self.dial) - 1: 
-            print(f'big: {self.current_number} and {rotation} result: {(rotation + self.current_number) % len(self.dial)}')
-            self.set_current_number(self.dial[(rotation + self.current_number) % len(self.dial)])
-        else:
-            print(f'small: {self.current_number} and {rotation}')
-            self.set_current_number(rotation + self.current_number)
-        if self.current_number == 0:
-            self.zeros_sum += 1
+        if rotation > 0:
+            passed_zeros = (end_pos // self.dial_size) - (start_pos // self.dial_size)
+        elif rotation < 0:
+            # -1 is for case when current_number is 0 and we move to the left. Thanks to that, function consider
+            # 0 as 99 and will not count zero incorrectly
+            passed_zeros = ((start_pos - 1) // self.dial_size) - ((end_pos - 1) // self.dial_size)
         
+        self.zeros_sum += passed_zeros
+
+        self.current_number = end_pos
--- day_1/solution.py
+++ day_1/solution.py
@@ -5,7 +5,6 @@ safe = SafeDial()
 with open("day_1/input.txt") as file:
     sequence = file.read().split("\n")
     sequence.remove(sequence[-1])
-    print(sequence)
 
 sequence = safe.parse(sequence)
 
@@ -13,3 +12,13 @@ for number in sequence:
     safe.rotate(number)
 
 print(safe.zeros_sum)
+
+# test_input = ["L68","L30","R48","L5","R60","L55","L1","L99","R14","L82"]
+# sequence = safe.parse(test_input)
+
+# for number in sequence:
+#     safe.rotate(number)
+
+# print(safe.zeros_sum)
+
+# 1975 is too low
